2c98720887c92734a52d3fdc9c0fc52a0e6cceb6
Filipe Corrêa <filipecorrea@br.ibm.com>
2019-07-08 16:06:12 -0300

    Fix unit tests

--- src/ImageHotspots.test.js
+++ src/ImageHotspots.test.js
@@ -1,28 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { act } from 'react-dom/test-utils'
+import { act, isDOMComponent } from 'react-dom/test-utils'
 import ImageHotspots from './ImageHotspots'
 
 describe('ImageHotspots', () => {
-  describe('when loading products', () => {
-    let container
+  let container
 
-    beforeEach(() => {
-      container = document.createElement('div')
-      document.body.appendChild(container)
-    })
+  beforeEach(() => {
+    container = document.createElement('div')
+    document.body.appendChild(container)
+  })
 
-    afterEach(() => {
-      document.body.removeChild(container)
-      container = null
-    })
+  afterEach(() => {
+    document.body.removeChild(container)
+    container = null
+  })
 
-    it('shows loading', () => {
-      act(() => {
-        ReactDOM.render(<ImageHotspots />, container)
-      })
-      const label = container.querySelector('p')
-      expect(label.textContent).toBe('ImageHotspots')
+  it('shows loading', () => {
+    act(() => {
+      ReactDOM.render(<ImageHotspots />, container)
     })
+    const image = container.querySelector('img')
+    expect(isDOMComponent(image)).toBe(true)
   })
 })
