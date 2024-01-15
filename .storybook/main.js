360817d0aa19e8929139322927e7768071fbcf56
Filipe Correa <filipecorrea@me.com>
2024-01-15 10:19:19 +0100

    chore: storybook 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,11 +3,20 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-controls",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react"
+
+  "framework": {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
 }
